5974203fdb6e6f42eca05fcf4cf60845ab71f4bc
dny343 <dnynshwrshinde007@gmail.com>
2024-08-28 14:13:48 +0530

    First update

--- scripts.js
+++ scripts.js
@@ -23,3 +23,28 @@ subMenuTitles.forEach((title) => {
     menu.classList.remove("submenu-active");
   });
 });
+
+const allowedUsers = ["dnyaneshwar@christmascompany.com", "david@christmascompany.com"];
+
+// Login Form Submission
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (allowedUsers.includes(email)) { // Replace with actual authentication logic
+        localStorage.setItem('userEmail', email);
+        window.location.href = "project-list.html";
+    } else {
+        errorMessage.textContent = "Invalid email or password.";
+    }
+});
+
+
+
+// Logout Function
+function logout() {
+    localStorage.removeItem('userEmail');
+    window.location.href = "index.html";
+}
